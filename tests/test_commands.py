4a26a0ec63fc86c9b3826f4686fe61446e5ee0ad
Shogo Sawai <shogo.sawai@gmail.com>
2017-05-15 21:16:54 +0900

    add test binary contents

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -5,6 +5,9 @@ from __future__ import print_function
 
 import os
 import time
+import shutil
+
+from six import binary_type
 
 from django.test import TestCase
 from django.conf import settings
@@ -23,7 +26,7 @@ class TestDjango_busybody_collectstatic(TestCase):
         if not os.path.exists(settings.STATICFILES_DIRS[0]):
             os.mkdir(settings.STATICFILES_DIRS[0])
 
-    def test_command_no_effect(self):
+    def test_command_base(self):
         verbosity = 0
         filename = os.path.join(settings.STATICFILES_DIRS[0], 'test')
         dst_filename = os.path.join(settings.STATIC_ROOT, 'test')
@@ -49,6 +52,25 @@ class TestDjango_busybody_collectstatic(TestCase):
         call_command('collectstatic_ext', interactive=False, verbosity=verbosity)
         self.assertTrue(os.path.exists(dst_filename))
         self.assertNotEqual(time.ctime(os.path.getmtime(dst_filename)), when_modified)
+
+    def test_command_contents(self):
+        verbosity = 0
+        contents_list = ['hello world', '日本語', b'hogehoge']
+        files = []
+        for index, contents in enumerate(contents_list):
+            filename = os.path.join(settings.STATICFILES_DIRS[0], 'test{}'.format(index))
+            dst_filename = os.path.join(settings.STATIC_ROOT, 'test{}'.format(index))
+            if isinstance(contents, binary_type):
+                with open(filename, 'wb') as fp:
+                    fp.write(contents)
+            else:
+                with open(filename, 'wb') as fp:
+                    fp.write(contents.encode('UTF-8'))
+            files.append((filename, dst_filename))
+
+        call_command('collectstatic_ext', interactive=False, verbosity=verbosity)
+        for _, dst_filename in files:
+            self.assertTrue(os.path.exists(dst_filename))
 
     def tearDown(self):
         settings.STATIC_ROOT = self.org_static_root
