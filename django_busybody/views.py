d23cca0e0744dfdc980578df62e3fb84da800a3c
Shogo Sawai <shogo.sawai@gmail.com>
2017-05-22 22:43:25 +0900

    add multiple form mixin

--- django_busybody/views.py
+++ django_busybody/views.py
@@ -7,12 +7,12 @@ import io
 
 from six.moves.urllib.parse import quote
 
-from django.views import generic
 from django.http.response import HttpResponse
+from django.views.generic.base import TemplateResponseMixin
+from django.views.generic.edit import FormMixin, ProcessFormView
+
 
 from .datas import CsvDataMixin, ZipDataMixin
-
-FormMixin = generic.edit.FormMixin
 
 
 class SearchFormMixin(FormMixin):
@@ -61,3 +61,56 @@ class DownloadCsvMixin(CsvDataMixin, DownloadMixin):
 
 class DownloadZipMixin(ZipDataMixin, DownloadMixin):
     pass
+
+
+# from https://gist.github.com/michelts/1029336
+class MultipleFormsMixin(FormMixin):
+    """
+    A mixin that provides a way to show and handle several forms in a
+    request.
+    """
+    form_classes = {}  # set the form classes as a mapping
+
+    def get_form_classes(self):
+        return self.form_classes
+
+    def get_forms(self, form_classes):
+        return dict([(key, klass(**self.get_form_kwargs()))
+                     for key, klass in form_classes.items()])
+
+    def forms_valid(self, forms):
+        return super(MultipleFormsMixin, self).form_valid(forms)
+
+    def forms_invalid(self, forms):
+        return self.render_to_response(self.get_context_data(forms=forms))
+
+
+class ProcessMultipleFormsView(ProcessFormView):
+    """
+    A mixin that processes multiple forms on POST. Every form must be
+    valid.
+    """
+    def get(self, request, *args, **kwargs):
+        form_classes = self.get_form_classes()
+        forms = self.get_forms(form_classes)
+        return self.render_to_response(self.get_context_data(forms=forms))
+
+    def post(self, request, *args, **kwargs):
+        form_classes = self.get_form_classes()
+        forms = self.get_forms(form_classes)
+        if all([form.is_valid() for form in forms.values()]):
+            return self.forms_valid(forms)
+        else:
+            return self.forms_invalid(forms)
+
+
+class BaseMultipleFormsView(MultipleFormsMixin, ProcessMultipleFormsView):
+    """
+    A base view for displaying several forms.
+    """
+
+
+class MultipleFormsView(TemplateResponseMixin, BaseMultipleFormsView):
+    """
+    A view for displaing several forms, and rendering a template response.
+    """
