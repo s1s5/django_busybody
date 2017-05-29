2baee12ab7480fd43f3dc273ed7318e981de92da
Shogo Sawai <shogo.sawai@gmail.com>
2017-05-30 00:17:03 +0900

    add multiple form

--- django_busybody/views.py
+++ django_busybody/views.py
@@ -10,7 +10,9 @@ from six.moves.urllib.parse import quote
 from django.http.response import HttpResponse
 from django.views.generic.base import TemplateResponseMixin
 from django.views.generic.edit import FormMixin, ProcessFormView
-
+from django.core.exceptions import ImproperlyConfigured
+from django.forms import models as model_forms
+from django.views.generic.base import ContextMixin
 
 from .datas import CsvDataMixin, ZipDataMixin
 
@@ -31,6 +33,153 @@ class SearchFormMixin(FormMixin):
                 'data': self.request.GET,
             })
         return kwargs
+
+
+class MultipleObjectMixin(ContextMixin):
+    model_map = {}
+    pk_url_kwarg = '{}_pk'
+
+    def get_objects(self):
+        return {
+            key: model._default_manager.all().get(
+                pk=self.kwargs.get(self.pk_url_kwarg.format(key)))
+            for key, model in self.model_map.items()
+        }
+
+
+class MultipleFormMixin(FormMixin):
+    initial_map = {}
+    form_class_map = {}
+    prefix_map = {}
+
+    def get_initial(self, key):
+        return self.initial_map.get(key, {})
+
+    def get_prefix(self, key):
+        return self.prefix_map.get(key, key)
+
+    def get_form_class(self, key):
+        return self.form_class_map[key]
+
+    def get_form(self, key, form_class=None):
+        """
+        Returns an instance of the form to be used in this view.
+        """
+        if form_class is None:
+            form_class = self.get_form_class(key)
+        return form_class(**self.get_form_kwargs(key))
+
+    def get_form_kwargs(self, key):
+        """
+        Returns the keyword arguments for instantiating the form.
+        """
+        kwargs = {
+            'initial': self.get_initial(key),
+            'prefix': self.get_prefix(key),
+        }
+
+        if self.request.method in ('POST', 'PUT'):
+            kwargs.update({
+                'data': self.request.POST,
+                'files': self.request.FILES,
+            })
+        return kwargs
+
+    def get_form_class_keys(self):
+        return self.form_class_map.keys()
+
+    def get_forms(self):
+        return {
+            key: self.get_form(key)
+            for key in self.get_form_class_keys()
+        }
+
+    def get_context_data(self, **kwargs):
+        """
+        Insert the form into the context dict.
+        """
+        for key in self.get_form_class_keys():
+            kwargs['{}_form'.format(key)] = self.get_form(key)
+        return super(FormMixin, self).get_context_data(**kwargs)
+
+
+class MultipleModelFormMixin(MultipleFormMixin, MultipleObjectMixin):
+    fields = {}
+
+    def get_form_class(self, key):
+        if self.fields.get(key) is not None and self.form_class_map.get(key):
+            raise ImproperlyConfigured(
+                "Specifying both 'fields' and 'form_class' is not permitted."
+            )
+        if self.form_class_map.get(key):
+            return self.form_class_map.get(key)
+        else:
+            if self.model_map.get(key) is not None:
+                # If a model has been explicitly provided, use it
+                model = self.model_map.get(key)
+            elif (hasattr(self, '{}_object'.format(key)) and
+                  getattr(self, '{}_object'.format(key)) is not None):
+                # If this view is operating on a single object, use
+                # the class of that object
+                model = self.object.__class__
+            else:
+                # Try to get a queryset and extract the model class
+                # from that
+                model = self.get_queryset().model
+
+            if self.fields is None:
+                raise ImproperlyConfigured(
+                    "Using ModelFormMixin (base class of %s) without "
+                    "the 'fields' attribute is prohibited." % self.__class__.__name__
+                )
+
+            return model_forms.modelform_factory(model, fields=self.fields.get(key))
+
+    def get_form_kwargs(self, key):
+        """
+        Returns the keyword arguments for instantiating the form.
+        """
+        kwargs = super(MultipleModelFormMixin, self).get_form_kwargs(key)
+        if hasattr(self, '{}_object'.format(key)):
+            kwargs.update({'instance': getattr(self, '{}_object'.format(key))})
+        return kwargs
+
+    def get_form_class_keys(self):
+        s = set(self.form_class_map.keys())
+        s.update(self.model_map.keys())
+        return list(s)
+
+    def form_valid(self, forms):
+        """
+        If the form is valid, save the associated model.
+        """
+        for key, form in forms.items():
+            setattr(self, '{}_object'.format(key), form.save())
+        return super(MultipleModelFormMixin, self).form_valid(forms)
+
+
+class ProcessMultipleFormView(ProcessFormView):
+    def post(self, request, *args, **kwargs):
+        forms = self.get_forms()
+        if max(x.is_valid() for x in forms.values()):
+            return self.form_valid(forms)
+        else:
+            return self.form_invalid(forms)
+
+
+class BaseMultipleFormView(MultipleFormMixin, ProcessMultipleFormView):
+    pass
+
+
+class MultipleFormView(TemplateResponseMixin, BaseMultipleFormView):
+    pass
+
+
+class MultipleUpdateView(MultipleModelFormMixin, TemplateResponseMixin, ProcessMultipleFormView):
+    def dispatch(self, *args, **kwargs):
+        for key, obj in self.get_objects().items():
+            setattr(self, '{}_object'.format(key), obj)
+        return super(MultipleUpdateView, self).dispatch(*args, **kwargs)
 
 
 class DownloadMixin(object):
@@ -62,56 +211,3 @@ class DownloadCsvMixin(CsvDataMixin, DownloadMixin):
 
 class DownloadZipMixin(ZipDataMixin, DownloadMixin):
     pass
-
-
-# from https://gist.github.com/michelts/1029336
-class MultipleFormsMixin(FormMixin):
-    """
-    A mixin that provides a way to show and handle several forms in a
-    request.
-    """
-    form_classes = {}  # set the form classes as a mapping
-
-    def get_form_classes(self):
-        return self.form_classes
-
-    def get_forms(self, form_classes):
-        return dict([(key, klass(**self.get_form_kwargs()))
-                     for key, klass in form_classes.items()])
-
-    def forms_valid(self, forms):
-        return super(MultipleFormsMixin, self).form_valid(forms)
-
-    def forms_invalid(self, forms):
-        return self.render_to_response(self.get_context_data(forms=forms))
-
-
-class ProcessMultipleFormsView(ProcessFormView):
-    """
-    A mixin that processes multiple forms on POST. Every form must be
-    valid.
-    """
-    def get(self, request, *args, **kwargs):
-        form_classes = self.get_form_classes()
-        forms = self.get_forms(form_classes)
-        return self.render_to_response(self.get_context_data(forms=forms))
-
-    def post(self, request, *args, **kwargs):
-        form_classes = self.get_form_classes()
-        forms = self.get_forms(form_classes)
-        if all([form.is_valid() for form in forms.values()]):
-            return self.forms_valid(forms)
-        else:
-            return self.forms_invalid(forms)
-
-
-class BaseMultipleFormsView(MultipleFormsMixin, ProcessMultipleFormsView):
-    """
-    A base view for displaying several forms.
-    """
-
-
-class MultipleFormsView(TemplateResponseMixin, BaseMultipleFormsView):
-    """
-    A view for displaing several forms, and rendering a template response.
-    """
