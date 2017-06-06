afb9b98b9559b62ac044fc7ebf5abc60e990664c
Shogo Sawai <shogo.sawai@gmail.com>
2017-06-06 21:56:48 +0900

    bug fix

--- django_busybody/templatetags/busybody.py
+++ django_busybody/templatetags/busybody.py
@@ -13,7 +13,7 @@ register = Library()
 @register.simple_tag
 def replace_query_param(url, attr, val):
     (scheme, netloc, path, params, query, fragment) = urlparse(url)
-    query_dict = QueryDict(query).copy()
+    query_dict = QueryDict(query.encode('UTF-8')).copy()
     query_dict[attr] = val
     query = query_dict.urlencode()
     return urlunparse((scheme, netloc, path, params, query, fragment))
